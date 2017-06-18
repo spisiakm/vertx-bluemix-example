ad16fa001da1748ad55891198807a7e36995a930
Martin Spisiak <mspisiak@redhat.com>
2017-06-18 13:35:03 +0200

    First commit

---
+++ src/main/java/pa181/mspisiak/vertx/HelloWorldEmbedded.java
@@ -0,0 +1,22 @@
+package pa181.mspisiak.vertx;
+
+import io.vertx.core.Vertx;
+
+/**
+ * @author Martin Spišiak (mspisiak@redhat.com) on 18/06/17.
+ */
+public class HelloWorldEmbedded {
+	public static void main(String[] args) {
+		String portProperty = System.getenv("PORT");
+		if(portProperty==null)
+			portProperty = System.getenv("VCAP_APP_PORT");
+		if(portProperty==null)
+			portProperty = "8080";
+		int port = Integer.parseInt(portProperty);
+
+		System.out.println("Starting on port: "+port);
+
+		// Create an HTTP server which simply returns "Hello World!" to each request.
+		Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(port);
+	}
+}
